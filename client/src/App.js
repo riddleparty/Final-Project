73c22dcbec2db2c3d8889b1105b63852bb99e4c0
email <phupeter916@gmail.com>
2018-11-10 12:46:22 -0800

    current

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Jumbotron from "./components/Jumbotron";
 
 class App extends Component {
   render() {
     return (
+      <div>
       <h1>Hello</h1>
+      <Jumbotron />
+      </div>
+
+
+
     );
   }
 }
